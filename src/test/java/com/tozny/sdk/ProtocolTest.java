585bd0a97f2c595ad62e9775366afda4311e5a99
James Bielman <jamesjb@galois.com>
2016-01-14 11:25:11 -0800

    Update ProtocolTest to JUnit 4 API.

--- src/test/java/com/tozny/sdk/ProtocolTest.java
+++ src/test/java/com/tozny/sdk/ProtocolTest.java
@@ -1,28 +1,12 @@
 package com.tozny.sdk;
 
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.io.IOException;
 
-public class ProtocolTest  extends TestCase {
-
-    public ProtocolTest(String testName)
-    {
-        super( testName );
-
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ProtocolTest.class );
-    }
-
+public class ProtocolTest {
+    @Test
     public void testCompareStrings() throws IOException {
         assertTrue(Protocol.compareStrings("one", "one"));
         assertTrue(Protocol.compareStrings("two", "two"));
@@ -30,6 +14,6 @@ public class ProtocolTest  extends TestCase {
         assertFalse(Protocol.compareStrings("two", "one"));
         assertFalse(Protocol.compareStrings("one", "onetwo"));
         assertFalse(Protocol.compareStrings("two", "onetwo"));
-        //TODO: Implement random comparisons
+        // TODO: Implement random comparisons
     }
 }
