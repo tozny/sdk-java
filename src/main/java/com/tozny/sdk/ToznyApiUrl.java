c33ceed83544d860e06ff69518f718dcceb7a0d2
James Bielman <jamesjb@galois.com>
2016-01-14 20:37:46 -0800

    Add trailing slash to Tozny API URL.
    - Fixes unit tests on JDK 7.

--- src/main/java/com/tozny/sdk/ToznyApiUrl.java
+++ src/main/java/com/tozny/sdk/ToznyApiUrl.java
@@ -8,7 +8,7 @@ import com.google.api.client.util.Key;
  * All Tozny API calls have a required 'method' property.
  */
 public class ToznyApiUrl extends GenericUrl {
-    public static final String TOZNY_PRODUCTION_API_URL = "https://api.tozny.com";
+    public static final String TOZNY_PRODUCTION_API_URL = "https://api.tozny.com/";
 
     public ToznyApiUrl(String toznyApiUrl, String method) {
         super(toznyApiUrl);
